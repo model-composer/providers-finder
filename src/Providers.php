860f1236f0920a44672941703b0f1c98cd22f6ee
Domenico Giambra <zorinik@gmail.com>
2022-09-01 00:21:42 +0200

    Fix duplicates

--- src/Providers.php
+++ src/Providers.php
@@ -7,9 +7,16 @@ class Providers
 	public static function find(string $className): array
 	{
 		$providers = [];
+		$seen = [];
+
 		foreach (InstalledVersions::getAllRawData() as $installedVersions) {
 			foreach ($installedVersions['versions'] as $package => $packageData) {
 				if (str_starts_with($package, 'model/')) {
+					if (in_array($package, $seen))
+						continue;
+
+					$seen[] = $package;
+
 					$namespaceName = ucfirst(preg_replace_callback('/[-_](.)/', function ($matches) {
 						return strtoupper($matches[1]);
 					}, substr($package, 6)));
